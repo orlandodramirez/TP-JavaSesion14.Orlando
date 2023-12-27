745fe5cdd7aa87ccdf8eebbe7904d9f062b66327
orlandodramirez <153769438+orlandodramirez@users.noreply.github.com>
2023-12-27 18:16:16 -0300

    Add files via upload

---
+++ sesion14Orlando/PRACTICA09/src/principal/Principal.java
@@ -0,0 +1,19 @@
+package principal;
+
+/**
+ * @author Orlando Ramirez
+ */
+
+import tienda.Login;
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login ventana1 = new Login();        
+    }
+    
+}
